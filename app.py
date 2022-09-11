1c7ef52b7f8eee74920ba70d08ac213b59da67d1
kmatiya <khormatiya@gmail.com>
2022-09-11 20:42:23 +0200

    Filter or search artist in db by id

--- app.py
+++ app.py
@@ -250,13 +250,20 @@ def search_artists():
     # TODO: implement search on artists with partial string search. Ensure it is case-insensitive.
     # seach for "A" should return "Guns N Petals", "Matt Quevado", and "The Wild Sax Band".
     # search for "band" should return "The Wild Sax Band".
-    response = {
-        "count": 1,
-        "data": [{
-            "id": 4,
-            "name": "Guns N Petals",
-            "num_upcoming_shows": 0,
-        }]
+    search_term = request.form.get('search_term', '')
+    search_artist = db.session.query(Artist).filter(Artist.name.ilike(f'%{search_term}%')).all()
+    data = []
+
+    for artist in search_artist:
+        data.append({
+        "id": artist.id,
+        "name": artist.name,
+        "num_upcoming_shows": len(db.session.query(Show).filter(Show.artist_id == artist.id).filter(Show.start_time > datetime.now()).all()),
+        })
+  
+    response={
+        "count": len(search_artist),
+        "data": data
     }
     return render_template('pages/search_artists.html', results=response,
                            search_term=request.form.get('search_term', ''))
