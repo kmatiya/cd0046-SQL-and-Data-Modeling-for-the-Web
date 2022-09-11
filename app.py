f8d2f84c9d27d43e8136f6fab033584f1183a697
kmatiya <khormatiya@gmail.com>
2022-09-11 17:18:03 +0200

    List artists from db

--- app.py
+++ app.py
@@ -261,16 +261,7 @@ def delete_venue(venue_id):
 @app.route('/artists')
 def artists():
     # TODO: replace with real data returned from querying the database
-    data = [{
-        "id": 4,
-        "name": "Guns N Petals",
-    }, {
-        "id": 5,
-        "name": "Matt Quevedo",
-    }, {
-        "id": 6,
-        "name": "The Wild Sax Band",
-    }]
+    data = Artist.query.order_by('name').all()
     return render_template('pages/artists.html', artists=data)
 
 
