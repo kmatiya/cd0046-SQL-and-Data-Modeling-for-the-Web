1bb7fd25e13f8a15bf584130996ccbaba33c9473
kmatiya <khormatiya@gmail.com>
2022-09-11 17:08:44 +0200

    Add show in db

--- app.py
+++ app.py
@@ -443,7 +443,6 @@ def create_artist_submission():
     # TODO: insert form data as a new Venue record in the db, instead
     # TODO: modify data to be the data object returned from db insertion
     error = False
-    error_message = ""
     try:
         name = request.form['name']
         city = request.form['city']
@@ -471,7 +470,7 @@ def create_artist_submission():
         db.session.close()
     if error:
         flash('An error occurred whilst creating Artist ' + request.form[
-            'name'] + '. Please try again or contact user support'+ str(error_message))
+            'name'] + '. Please try again or contact user support')
     if not error:
         # on successful db insert, flash success
         flash('Artist ' + request.form['name'] + ' was successfully listed!')
@@ -535,15 +534,30 @@ def create_shows():
 def create_show_submission():
     # called to create new shows in the db, upon submitting new show listing form
     # TODO: insert form data as a new Show record in the db, instead
+    error = False
+    try:
+        artist_id = request.form['artist_id']
+        venue_id = request.form['venue_id']
+        start_time = request.form['start_time']
 
-    # on successful db insert, flash success
-    flash('Show was successfully listed!')
-    # TODO: on unsuccessful db insert, flash an error instead.
-    # e.g., flash('An error occurred. Show could not be listed.')
-    # see: http://flask.pocoo.org/docs/1.0/patterns/flashing/
+        show = Show(artist_id=artist_id, venue_id=venue_id, start_time=start_time)
+        db.session.add(show)
+        db.session.commit()
+    except Exception as e:
+        error = True
+        db.session.rollback()
+        print(e)
+        print(sys.exc_info())
+    finally:
+        db.session.close()
+    if error:
+        flash('An error occurred whilst creating show. Please try again or contact user support')
+    if not error:
+        # on successful db insert, flash success
+        flash('Show was successfully listed!')
     return render_template('pages/home.html')
 
-
+    
 @app.errorhandler(404)
 def not_found_error(error):
     return render_template('errors/404.html'), 404
